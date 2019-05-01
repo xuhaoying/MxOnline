ff4746c0bf498f59480dd8df37f16012b7233e74
xuhaoying <xvhaoying@outlook.com>
2019-05-01 22:49:34 +0800

    增加登录页面

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -19,10 +19,11 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 import xadmin
 
+from users.views import *
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
-    url(r'^login/$', TemplateView.as_view(template_name="login.html"), name='login'),
+    url(r'^login/$', login, name='login'),
 
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = authenticate(username, password)
+        if user is not None:
+            login(request, user)
+            return render(request, "index.html")
+
+    elif request.method == 'GET':
+        return render(request, 'login.ht ml', locals())
